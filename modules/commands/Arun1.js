f7584e8ba1754281aa93f6338aec38b6b5844988
kumarsahab023 <159441524+kumarsahab023@users.noreply.github.com>
2024-03-31 08:29:09 +0530

    Update Arun1.js

--- modules/commands/Arun1.js
+++ modules/commands/Arun1.js
@@ -1,24 +1,24 @@
- const fs = require("fs");
+const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
+	name: "#OWNER",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Arun", 
+	credits: "Candy", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
-    cooldowns: 100, 
+    cooldowns: 1, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("owner") ||
-     react.includes("Arun") || 
-react.includes("arun")) {
+     react.includes("vivek sahab") || 
+react.includes("vivek")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐀𝐫𝐮𝐧 𝐊𝐮𝐦𝐚𝐫✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
-				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦𝐯𝐢𝐯𝐞𝐤𝐬𝐚𝐡𝐚𝐛✦\n\n☞\n\n★★᭄𝗖𝗿𝗲𝗱𝗶𝘁'𝘀 :  ✦𝒗𝒊𝒗𝒆𝒌❦✦`",
+				attachment: fs.createReadStream(__dirname + `/noprefix/vivek.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("📷", event.messageID, (err) => {}, true)
@@ -26,4 +26,4 @@ react.includes("arun")) {
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-    }
+	}
